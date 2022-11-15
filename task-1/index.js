710cb2f5869a6cddcd2905590b7b644a6b5eb456
nxiiln <barbashin.nv@gmail.com>
2022-11-16 00:34:30 +0300

    Создано createTimerAnimator

--- task-1/index.js
+++ task-1/index.js
@@ -5,7 +5,31 @@ const timerEl = document.querySelector('span');
 // Напишите реализацию createTimerAnimator
 // который будет анимировать timerEl
 const createTimerAnimator = () => {
-  return (seconds) => {};
+  return (seconds) => {
+    let currTime = 0;
+
+    const calculateTimeFormat = () => {
+      const ss = currTime < 60 ? currTime : currTime % 60;
+      const mm = (currTime >= 60 && currTime < 3600) ? ~~(currTime / 60) :
+        currTime > 3600 ? (~~(currTime / 60)) % 60 : 0;
+      const hh = currTime < 3600 ? 0 : ~~(currTime / 3600);
+
+      const getTimeFormat = time => time <= 9 ? `0${time}` : time;
+      return `${getTimeFormat(hh)}:${getTimeFormat(mm)}:${getTimeFormat(ss)}`;
+    }
+
+    const timer = () => {
+      if (currTime < seconds) {
+        currTime += 1;
+        timerEl.innerHTML = calculateTimeFormat();
+      }
+    }
+
+    timerEl.innerHTML = calculateTimeFormat();
+    const interval = setInterval(timer, 1000);
+    inputEl.addEventListener('input', () => clearInterval(interval));
+    if (currTime > seconds) clearInterval(interval);
+  };
 };
 
 const animateTimer = createTimerAnimator();
@@ -13,6 +37,7 @@ const animateTimer = createTimerAnimator();
 inputEl.addEventListener('input', () => {
   // Очистите input так, чтобы в значении
   // оставались только числа
+  inputEl.value = inputEl.value.replace(/\D/, '');
 });
 
 buttonEl.addEventListener('click', () => {
